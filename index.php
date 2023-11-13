93a63a79e31867475019b5c6915d33d1dbebb01b
luccas03x <136741892+luccas03x@users.noreply.github.com>
2023-11-13 16:33:38 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+<html>
+    <head>
+        <title> Andrade 3 Park © | 2020 </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    </head>
+
+    <body id="home">
+
+        <!-- Navbar -->
+        <div class="w3-top">
+            <div class="w3-bar w3-theme-d2 w3-left-align">
+                <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-hover-white w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
+                <a href="#home" class="w3-bar-item w3-button w3-yellow"><i class="fa fa-home w3-margin-right"></i>Home</a>
+                <a href="#equipe" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Equipe</a>
+                <a href="#area" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Sobre</a>
+                <a href="#preco" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Preço</a>
+                <a href="#contato" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Contato</a>
+            </div>
+        </div>
+
+        <!-- Banner -->
+        <div class="w3-display-container w3-animate-opacity">
+            <img src="img/Banner Andrade 3 Park.png" style="width:100%; height: 750px;">
+
+        </div>
+
+        <!-- Equipe -->
+        <div class="w3-container w3-padding-64 w3-center" id="equipe">
+            <h2>EQUIPE</h2>
+            <h4><i>Conheça nossa equipe:</i></h4>
+
+            <div class="w3-row"><br>
+
+                <div class="w3-quarter">
+                    <img src="img/valdemar.jpeg" style="width:45%" class="w3-circle w3-hover-opacity">
+                    <h3>Valdemar Andrade</h3>
+                    <p>Chefe</p>
+                </div>
+
+                <div class="w3-quarter">
+                    <img src="img/tereza.jpeg" style="width:45%" class="w3-circle w3-hover-opacity">
+                    <h3>Tereza Andrade</h3>
+                    <p>Chefe</p>
+                </div>
+
+                <div class="w3-quarter">
+                    <img src="img/neto.jpeg" style="width:45%" class="w3-circle w3-hover-opacity">
+                    <h3>Neto Machado Andrade</h3>
+                    <p>Chefe</p>
+                </div>
+
+                <div class="w3-quarter">
+                    <img src="img/summa.jpeg" style="width:45%" class="w3-circle w3-hover-opacity">
+                    <h3>Marcos Summa</h3>
+                    <p>Gerente</p>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- Sobre -->
+        <div class="w3-row-padding w3-padding-64 w3-theme-l1" id="area">
+
+            <div class="w3-quarter">
+                <h2>SOBRE</h2>
+                <br>
+                <h4 align="justify">Inaugurado em Julho de 2017, o estacionamento Andrade 3 Park teve a ideia de aproveitar um terreno de aproximadamente 1700m² com antigas casas (vilas) e transformar num estacionamento com uma região carente de espaço.</h4>
+                <br>
+                <h4 align="justify">Localizado na rua Werner von Siemens 64 em frente ao centro empresarial e-business na Lapa de baixo com horário de funcionamento Segunda-feira à Sexta-feira das 7:00 às 20:00hrs.</h4>
+            </div>
+
+            <div class="w3-quarter">
+                <div class="w3-card w3-white">
+                    <img src="img/entrada.jpeg" style="width:100%">
+                    <div class="w3-container">
+                        <center><i><h3>Entrada</h3></i></center>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w3-quarter">
+                <div class="w3-card w3-white">
+                    <img src="img/meio.jpeg" style="width:100%">
+                    <div class="w3-container">
+                        <center><i><h3>Vagas</h3></i></center>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w3-quarter">
+                <div class="w3-card w3-white">
+                    <img src="img/fundos.jpeg" style="width:100%">
+                    <div class="w3-container">
+                        <center><i><h3>Fundos</h3></i></center>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Preço -->
+        <div class="w3-row-padding w3-center w3-padding-64" id="preco">
+            <h2>PREÇO</h2>
+            <h4><i>Tabela de preços ao estacionar</i></h4><br>
+            <div class="w3-third w3-margin-bottom">
+                <ul class="w3-ul w3-border w3-hover-shadow">
+                    <li class="w3-theme">
+                        <p class="w3-xlarge">Dinheiro</p>
+                    </li>
+                    <li class="w3-padding-16"><b>1/2 hora:</b> R$ 10,00</li>
+                    <li class="w3-padding-16"><b>1 hora:</b> R$ 15,00</li>
+                    <li class="w3-padding-16"><b>2 horas:</b> R$ 25,00</li>
+                    <li class="w3-padding-16"><b>3 horas:</b> R$ 28,00</li>
+                    <li class="w3-padding-16">
+                        <h2><b>R$</b> 28</h2>
+                        <span class="w3-opacity">Diária</span>
+                    </li>
+                    <li class="w3-theme-l5 w3-padding-24">
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="w3-third w3-margin-bottom">
+                <ul class="w3-ul w3-border w3-hover-shadow">
+                    <li class="w3-yellow">
+                        <p class="w3-xlarge">Crédito</p>
+                    </li>
+                    <li class="w3-padding-16"><b>1/2 hora:</b> R$ 11,00</li>
+                    <li class="w3-padding-16"><b>1 hora:</b> R$ 16,00</li>
+                    <li class="w3-padding-16"><b>2 horas:</b> R$ 27,00</li>
+                    <li class="w3-padding-16"><b>3 horas:</b> R$ 30,00</li>
+                    <li class="w3-padding-16">
+                        <h2><b>R$</b> 30</h2>
+                        <span class="w3-opacity">Diária</span>
+                    </li>
+                    <li class="w3-theme-l5 w3-padding-24"></li>
+                </ul>
+            </div>
+            
+            <div class="w3-third w3-margin-bottom">
+                <ul class="w3-ul w3-border w3-hover-shadow">
+                    <li class="w3-theme">
+                        <p class="w3-xlarge">Débito</p>
+                    </li>
+                    <li class="w3-padding-16"><b>1/2 hora:</b> R$ 10,00</li>
+                    <li class="w3-padding-16"><b>1 hora:</b> R$ 15,00</li>
+                    <li class="w3-padding-16"><b>2 horas:</b> R$ 25,00</li>
+                    <li class="w3-padding-16"><b>3 horas:</b> R$ 28,00</li>
+                    <li class="w3-padding-16">
+                        <h2><b>R$</b> 28</h2>
+                        <span class="w3-opacity">Diária</span>
+                    </li>
+                    <li class="w3-theme-l5 w3-padding-24">
+                    </li>
+                </ul>
+            </div>
+
+            
+        </div>
+
+        <!-- Contato -->
+    <center>
+        <div class="w3-container w3-padding-64 w3-theme-l5" id="contato">
+            <div class="w3-row">
+                <div class="w3-padding-16"><span class="w3-xlarge w3-border-black w3-bottombar">Entre em contato</span></div>
+                <h3>Estacione das 7:00 às 20:00.</h3>
+                <h5>Rua Werner von Siemes, 64</h5>
+                <p><i class="fa fa-map-marker w3-text-yellow w3-xlarge"></i>  São Paulo, Brasil</p>
+                <p><i class="fa fa-phone w3-text-yellow w3-xlarge"></i>  +11 97337-2442</p>
+                <p><i class="fa fa-envelope-o w3-text-yellow w3-xlarge"></i>  andrade3park@gmail.com</p>
+            </div>
+
+        </div>
+    </center>
+
+    <!-- Rodapé -->
+    <footer class="w3-container w3-padding-32 w3-theme-d1 w3-center">
+        <p>© 2020 | Andrade 3 Park | Desenvolvido por Alloy</p>
+
+        <!-- colocar ir pro top -->
+
+    </footer>
+
+    <script>
+        // Script for side navigation
+        function w3_open() {
+            var x = document.getElementById("mySidebar");
+            x.style.width = "300px";
+            x.style.paddingTop = "10%";
+            x.style.display = "block";
+        }
+
+        // Close side navigation
+        function w3_close() {
+            document.getElementById("mySidebar").style.display = "none";
+        }
+
+        // Used to toggle the menu on smaller screens when clicking on the menu button
+        function openNav() {
+            var x = document.getElementById("navDemo");
+            if (x.className.indexOf("w3-show") == -1) {
+                x.className += " w3-show";
+            } else {
+                x.className = x.className.replace(" w3-show", "");
+            }
+        }
+    </script>
+
+</body>
+</html>
+
